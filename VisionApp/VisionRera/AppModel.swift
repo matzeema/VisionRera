204b12cb43f436e203bf9f27ce5a54a3c8cce599
matzesoft <matzesoft@gmail.com>
2024-10-09 18:29:22 +0200

    Create xCode project

---
+++ VisionApp/VisionRera/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  VisionRera
+//
+//  Created by Mattias Emanuel on 09.10.24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
