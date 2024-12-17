19bc320e3208fb56fa8259b0914fb14aef116f83
matzesoft <matzesoft@gmail.com>
2024-12-17 16:48:01 +0100

    Remove GameMode .none case & add comments

--- VisionApp/VisionRera/Game/GameModel.swift
+++ VisionApp/VisionRera/Game/GameModel.swift
@@ -5,13 +5,18 @@
 //  Created by Mattias Emanuel on 05.11.24.
 //
 
+import Foundation
 import SwiftUI
 
+/// The state of the speed of the user controlled car. The slot of the RaceTrack is
+/// defined in the RaceTrackModel via the `defaultSlot`.
 enum SpeedToRacetrackState {
     case stop
     case racing(speed: Float)
 }
 
+/// The protocol every game mode has to conform to. Provides default options like
+/// the current speed state.
 protocol GameModeProtocol {
     var speedToRaceTrackState: SpeedToRacetrackState { get set }
 }
@@ -20,26 +25,39 @@ protocol GameModeProtocol {
 @MainActor
 @Observable
 class GameModel {
+    private(set) var gameModeHandler: (any GameModeProtocol)?
     
-    enum Mode {
-        case none
+    enum Mode: String, CaseIterable {
         case freeDrive
         case lapsRace
     }
-    var mode: Mode = .none {
+    
+    var mode: Mode? = nil {
         didSet {
             switch mode {
-            case .none:      gameModeHandler = nil
+            case nil: gameModeHandler = nil
             case .freeDrive: gameModeHandler = FreeDriveMode()
             case .lapsRace:  gameModeHandler = LapsRaceMode()
             }
         }
     }
     
-    private(set) var gameModeHandler: (any GameModeProtocol)?
-    
     var lapSessionFeature: LapSessionProtocol? {
         gameModeHandler as? LapSessionProtocol
     }
+}
+
+/// Gives metadata about the GameModes like the name and description of the mode.
+extension GameModel.Mode {
+    struct Metadata {
+        let name: String
+        let description: String
+    }
     
+    var metadata: Metadata {
+        switch self {
+            case .freeDrive: return Metadata(name: "Free Drive", description: "Drive around the track")
+            case .lapsRace:  return Metadata(name: "Laps Race", description: "Race around the track")
+        }
+    }
 }
