036ddc76d8bfabe2ff54c919688b627c81115c52
matzesoft <matzesoft@gmail.com>
2024-12-17 21:29:55 +0100

    Create basic FreeDriveGameView

--- VisionApp/VisionRera/Views/Game/GameView.swift
+++ VisionApp/VisionRera/Views/Game/GameView.swift
@@ -9,36 +9,72 @@ import SwiftUI
 
 /// Shows the controls like stopping the game for the current GameMode.
 struct GameView: View {
-    @Environment(GameModel.self) private var gameMode
+    @Environment(GameModel.self) private var gameModel
+    
+    var body: some View {
+        VStack {
+            switch gameModel.mode {
+            case .freeDrive: FreeDriveView()
+            case .lapsRace: Text("Laps Race")
+                
+            default: EmptyView()
+            }
+        }
+        .gradientBackground(color: .indigo)
+    }
+}
+
+private struct FreeDriveView: View {
+    @Environment(GameModel.self) private var gameModel
     @State private var showingAlert = false
     
     var body: some View {
         VStack {
-            Text(gameMode.mode?.metadata.name ?? "Unknown Game Mode")
-                .font(.largeTitle)
-                .padding(12.0)
-            
-            
-            
-            Button("Exit game", systemImage: "xmark", action: {
-                showingAlert = true
-            })
-            .padding(12.0)
-            .alert(
-                "Exit game",
-                isPresented: $showingAlert,
-                actions: {
-                    Button("Exit", role: .destructive) {
-                        gameMode.mode = nil
+            HStack {
+                Text(gameModel.mode?.metadata.name ?? "Free Drive")
+                .font(.title)
+                .padding(.horizontal, 18.0)
+                .padding(.vertical, 6.0)
+                
+                Spacer()
+                
+                Button("Exit", systemImage: "xmark", action: {
+                    showingAlert = true
+                })
+                .padding(6.0)
+                .alert(
+                    "Exit game",
+                    isPresented: $showingAlert,
+                    actions: {
+                        Button("Exit", role: .destructive) {
+                            gameModel.mode = nil
+                        }
+                        Button("Cancel", role: .cancel) {}
                     }
-                    Button("Cancel", role: .cancel) {}
-                },
-                message: {
-                    Text("No highscores will be stored.")
+                )
+            }
+            .padding(16.0)
+            
+            if let laps = gameModel.lapSessionFeature?.lapSessionFeature.laps {
+            
+                Divider()
+                
+                ScrollView {
+                    ForEach(laps) { lap in
+                        HStack {
+                            Text("Lap \(lap.id)")
+                                .font(.headline)
+                                .padding(.leading, 16.0)
+                                .padding(.vertical, 8.0)
+                            
+                            Text("\(lap.durationInMillis ?? 0) sek")
+                        }
+                    }
                 }
-            )
+                .foregroundStyle(.secondary)
+                
+            }
         }
-        .gradientBackground(color: .indigo)
     }
 }
 
