f6404f5052fee8a43ad20d7a67345d0d30bbb3a2
matzesoft <matzesoft@gmail.com>
2024-12-19 13:28:59 +0100

    Optimize FreeDrive View

--- VisionApp/VisionRera/Views/Game/GameView.swift
+++ VisionApp/VisionRera/Views/Game/GameView.swift
@@ -14,7 +14,12 @@ struct GameView: View {
     var body: some View {
         VStack {
             switch gameModel.mode {
-            case .freeDrive: FreeDriveView()
+            case .freeDrive:
+                FreeDriveView(
+                    name: gameModel.mode?.metadata.name ?? "Free Drive",
+                    laps: gameModel.lapSessionFeature?.lapSessionFeature.laps ?? [],
+                    exitGameAction: { gameModel.mode = nil }
+                )
             case .lapsRace: Text("Laps Race")
                 
             default: EmptyView()
@@ -25,60 +30,73 @@ struct GameView: View {
 }
 
 private struct FreeDriveView: View {
-    @Environment(GameModel.self) private var gameModel
     @State private var showingAlert = false
     
+    let name: String
+    let laps: [Lap]
+    let exitGameAction: () -> Void
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             HStack {
-                Text(gameModel.mode?.metadata.name ?? "Free Drive")
-                .font(.title)
-                .padding(.horizontal, 18.0)
-                .padding(.vertical, 6.0)
+                Text(name)
+                    .font(.title)
+                    .padding(.horizontal, 18.0)
+                    .padding(.vertical, 4.0)
                 
                 Spacer()
                 
                 Button("Exit", systemImage: "xmark", action: {
                     showingAlert = true
                 })
-                .padding(6.0)
+                .padding(.horizontal, 6.0)
                 .alert(
                     "Exit game",
                     isPresented: $showingAlert,
                     actions: {
-                        Button("Exit", role: .destructive) {
-                            gameModel.mode = nil
-                        }
+                        Button("Exit", role: .destructive, action: exitGameAction)
                         Button("Cancel", role: .cancel) {}
                     }
                 )
             }
             .padding(16.0)
             
-            if let laps = gameModel.lapSessionFeature?.lapSessionFeature.laps {
+            Divider()
             
-                Divider()
-                
-                ScrollView {
-                    ForEach(laps) { lap in
-                        HStack {
-                            Text("Lap \(lap.id)")
-                                .font(.headline)
-                                .padding(.leading, 16.0)
-                                .padding(.vertical, 8.0)
-                            
-                            Text("\(lap.durationInMillis ?? 0) sek")
+            ScrollView {
+                VStack {
+                    ForEach(laps.sorted(by: { a,b in a.id > b.id })) { lap in
+                        if lap.isFinished {
+                            HStack {
+                                Text("Lap \(lap.id)")
+                                    .font(.headline)
+                                    .padding(.vertical, 4.0)
+                                    .padding(.trailing, 8.0)
+                                
+                                Text("\(lap.durationInMillis ?? 0) sek")
+                            }
+                            .frame(maxWidth: .infinity)
                         }
                     }
                 }
-                .foregroundStyle(.secondary)
-                
+                .padding()
             }
+            .foregroundStyle(.secondary)
         }
     }
 }
 
-#Preview(windowStyle: .automatic, traits: .fixedLayout(width: 400, height: 250)) {
-    GameView()
-        .environment(GameModel())
+#Preview(windowStyle: .automatic, traits: .fixedLayout(width: 600, height: 400)) {
+    FreeDriveView(
+        name: "Free Drive",
+        laps: [
+            Lap(id: 1, startMillis: 0, endMillis: 2000),
+            Lap(id: 2, startMillis: 2000, endMillis: 2357),
+            Lap(id: 3, startMillis: 2357, endMillis: 2942),
+            Lap(id: 4, startMillis: 2942, endMillis: 3447),
+            Lap(id: 5, startMillis: 3447, endMillis: 4001),
+            Lap(id: 6, startMillis: 3447)
+        ],
+        exitGameAction: {}
+    )
 }
