177234adec7fc324ada1aaad55aec026cd87e7c6
matzesoft <matzesoft@gmail.com>
2025-05-05 18:57:07 +0200

    fix: MainMenu Preview

--- VisionApp/VisionRera/Views/MainMenu/MainMenuView.swift
+++ VisionApp/VisionRera/Views/MainMenu/MainMenuView.swift
@@ -39,4 +39,5 @@ struct MainMenuView: View {
         .environment(RaceTrackModel())
         .environment(InputModel())
         .environment(GameModel())
+        .environment(TrackDetectionModel())
 }
